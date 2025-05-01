30c20f5c4368a97bec7097069eb8e7cc83aa19cd
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2025-04-30 22:41:57 -0300

    create project files

---
+++ src/test/java/com/dms/dmssensors/temperature/monitoring/TemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.dmssensors.temperature.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
