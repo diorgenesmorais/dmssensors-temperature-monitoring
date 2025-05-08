aad96d2662ea1257b6f91c049e27a42537cade2f
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2025-05-07 21:25:36 -0300

    feat: add validation to enable sensor method to prevent re-enabling already enabled sensors

--- src/main/java/com/dms/dmssensors/temperature/monitoring/api/controller/SensorMonitoringController.java
+++ src/main/java/com/dms/dmssensors/temperature/monitoring/api/controller/SensorMonitoringController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/monitoring")
@@ -47,6 +48,9 @@ public class SensorMonitoringController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void enable(@PathVariable TSID sensorId) {
         SensorMonitoring sensorMonitoring = findByIdOrDefault(sensorId);
+        if (Boolean.TRUE.equals(sensorMonitoring.getEnabled())) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
         sensorMonitoring.setEnabled(true);
         sensorMonitoringRepository.saveAndFlush(sensorMonitoring);
     }
