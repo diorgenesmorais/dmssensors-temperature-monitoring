5d529dd843f8d905819ada02493b711ced2d4eb6
Diorgenes M. Silva <diorgenesmorais@gmail.com>
2025-05-15 17:07:38 -0300

    feat: implement RabbitMQ configuration and message handling for temperature processing

---
+++ src/main/java/com/dms/dmssensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,37 @@
+package com.dms.dmssensors.temperature.monitoring.infrastructure.rabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    Queue temperatureMonitoringQueue() {
+        return QueueBuilder.durable("temperature-monitoring.process-temperature.v1.q")
+                .build();
+    }
+
+    FanoutExchange exchange() {
+        return ExchangeBuilder.fanoutExchange("temperature-processing.temperature-received.v1.e")
+                .build();
+    }
+
+    @Bean
+    Binding binding() {
+        return BindingBuilder.bind(temperatureMonitoringQueue()).to(exchange());
+    }
+}
